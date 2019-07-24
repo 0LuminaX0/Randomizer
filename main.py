5510061c102a93031808325812a221047e69949b
Andrii Negrub <zurg0505@gmail.com>
2019-07-24 15:29:24 +0300

    Added animation

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 from random import randint
+from time import sleep
+from os import system
 
-print(randint(0, 9))
+
+for i in range(10):
+    system("cls")
+    print(randint(0, 9))
+    sleep(0.1 * i)
